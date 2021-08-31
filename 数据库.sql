ae8eb47eda212f1c1a00b9837c6ffb9edac7bea9
gensokoy <83489418+gensokoy@users.noreply.github.com>
2021-08-31 19:51:09 +0800

    Add files via upload

---
+++ 数据库.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE excels;
+CREATE TABLE 数据(
+id VARCHAR(100),
+dat VARCHAR(30000),
+ExcelId VARCHAR(100),
+k INT
+);
+CREATE TABLE head(
+ExcelId VARCHAR(100),
+headdat VARCHAR(1000)
+);
